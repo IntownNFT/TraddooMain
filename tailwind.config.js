036a1b1a5fa8dfb546826a7ae356e57fad67f854
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-13 16:20:10 +0500

    Configure colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black": "#0F0F0F",
+        "dark-grey": "#141414",
+        "light-grey": "#252525",
+        "blue": "#3671E9",
+        "light-blue": "#4C94FF",
+        "red": "#EFA411",
+        "light-green": "#FFF281",
+        "off-white": "#FCFCFC"
+      }
+    },
   },
   plugins: [],
 }
