050c7c78a0fe9bb98e9566956017fe8b0a498443
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-13 21:06:05 +0500

    Edit layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="max-w-[1300px] w-full mx-auto">
+      <div className="max-w-[1300px] w-full mx-auto px-5 2xl:px-0">
         <Outlet />
       </div>
       Footer
