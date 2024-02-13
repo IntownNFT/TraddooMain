22434464e437de71bff46c4ac4cda586a4a87c22
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-13 19:52:19 +0500

    Configure home page [Landing-Pages]

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className="max-w-[1300px] w-full mx-auto">
+        <Outlet />
+      </div>
       Footer
     </>
   );
