eaf7dd755e8d560d9a4baf1000370d904857480d
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-15 01:15:03 +0500

    Edit squeezebox

--- src/components/SqueezeBox.tsx
+++ src/components/SqueezeBox.tsx
@@ -3,7 +3,7 @@ import {
   AccordionHeader,
   AccordionBody,
 } from "@material-tailwind/react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import ArrowIcon from "../assets/landing-page/arrow.svg";
 
 interface SqueezeboxData {
@@ -15,6 +15,17 @@ const Squeezebox = ({ title, description }: SqueezeboxData) => {
 
   const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
 
+  const renderWithLineBreaks = (text:string) => {
+    const textWithLineBreaks = text.split('\n').map((item, key) => (
+      <Fragment key={key}>
+        {item}
+        <br />
+      </Fragment>
+    ));
+
+    return <div>{textWithLineBreaks}</div>;
+  };
+
   return (
     <>
       <Accordion
@@ -25,19 +36,19 @@ const Squeezebox = ({ title, description }: SqueezeboxData) => {
         <AccordionHeader
           placeholder={""}
           onClick={() => handleOpen(1)}
-          className="flex justify-between items-center border-b-0 hover:text-off-white p-0"
+          className="relative flex justify-between items-center border-b-0 hover:text-off-white p-0"
         >
-          <h2 className="font-inter font-medium text-lg text-white w-full">{title}</h2>
+          <h2 className="font-inter font-medium text-base sm:text-lg text-white">{title}</h2>
           <img
             src={ArrowIcon}
             width={19}
             height={7}
-            className={open === 1 ? "rotate-0" : "rotate-180"}
+            className={`absolute right-0 ${open === 1 ? "rotate-0" : "rotate-180"}`}
             alt="Traddoo arrow"
           />
         </AccordionHeader>
-        <AccordionBody className="pt-10 font-kanit font-normal text-xl text-white">
-          {description}
+        <AccordionBody className="pt-10 font-kanit font-normal text-base sm:text-xl text-white leading-[36px]">
+          {renderWithLineBreaks(description)}
         </AccordionBody>
       </Accordion>
     </>
