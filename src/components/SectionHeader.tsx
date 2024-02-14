6a1e3f083722aa4cc7f22f0cf5b8d14ec9bca74d
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-14 21:05:28 +0500

    Edit SectionHeader component

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,19 +1,33 @@
 interface HeaderProps {
-    title: string,
-    subtitle: string,
-    id?: string,
+  title: string;
+  subtitle: string;
+  titleStyles?: string;
+  subtitleStyles?: string;
+  id?: string;
 }
-const SectionHeader = ({title, subtitle, id}:HeaderProps) => {
+const SectionHeader = ({
+  title,
+  subtitle,
+  id,
+  titleStyles,
+  subtitleStyles,
+}: HeaderProps) => {
   return (
     <>
-      <h2 className="font-raleway font-bold text-5xl text-center -tracking-[2.75px] text-balance leading-[55px]" id={id}>
+      <h2
+        className={`font-raleway font-bold text-5xl text-center -tracking-[2.75px] text-balance leading-[55px] ${titleStyles}`}
+        id={id}
+      >
         {title}
       </h2>
-      <p className="font-inter text-lg text-center text-[#A7AFBB] max-w-[670px] mx-auto mb-5" id={id}>
+      <p
+        className={`font-inter text-lg text-center text-[#A7AFBB] max-w-[670px] mx-auto mb-5 ${subtitleStyles}`}
+        id={id}
+      >
         {subtitle}
       </p>
     </>
-  )
-}
+  );
+};
 
-export default SectionHeader
+export default SectionHeader;
