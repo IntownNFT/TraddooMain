6567d88ade186e7d46915336f85e6e14d7950538
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-19 19:44:26 +0500

    Change newsletter styles

--- src/pages/landing-page/home/Newsletter.tsx
+++ src/pages/landing-page/home/Newsletter.tsx
@@ -1,38 +1,40 @@
-import FormButton from "../../../components/FormButton";
+import { Button, Input } from "@material-tailwind/react";
 import { useState } from "react";
 
 const Newsletter = () => {
   const [email, setEmail] = useState("");
 
   return (
-    <div className="py-16 px-5 bg-dark-grey border border-light-grey rounded-lg glowable-text">
-      <div>
-        <h3 className="font-raleway font-bold text-center text-3xl md:text-5xl">
-          Join Our Newsletter{" "}
-          <span className="text-blue text-3xl md:text-5xl translate-y-2 material-symbols-rounded">
-            mark_email_unread
-          </span>
-        </h3>
-        <p className="mt-5 mb-12 font-inter text-xl text-center max-w-xl mx-auto text-balance text-[#717171]">
-          Experience the best execution with tight spreads and a multi trading
-          platform experience. Join our newsletter today!
-        </p>
-      </div>
-      <div className="flex justify-center gap-3 mt-6 flex-wrap">
-        <input
+    <div className="py-24 px-5 bg-dark-grey border border-light-grey rounded-lg space-y-4">
+      <h3 className="font-raleway font-bold text-center text-3xl md:text-5xl">
+        Join Our Newsletter{" "}
+        <span className="text-blue text-3xl md:text-5xl translate-y-2 material-symbols-rounded">
+          mark_email_unread
+        </span>
+      </h3>
+      <div className="relative flex w-full max-w-lg mx-auto">
+        <Input
           type="email"
-          placeholder="john@email.com"
-          className="w-full max-w-2xl bg-transparent px-3 py-2 text-lg border-2 border-[#717171] focus:border-blue transition-colors rounded-md placeholder:text-light-grey focus:outline-none"
+          label="Email Address"
           value={email}
+          placeholder="John@email.com"
           onChange={(e) => setEmail(e.target.value)}
+          className="pr-20 bg-transparent px-3 py-2 text-lg text-white rounded-md border-2 !border-light-grey focus:!border-blue placeholder:!opacity-100 placeholder:!text-light-grey"
+          containerProps={{
+            className: "min-w-0",
+          }}
+          labelProps={{ className: "hidden" }}
           required
-          title="email"
+          crossOrigin={""}
         />
-        <FormButton
-          type="submit"
-          text="Join"
-          classes="max-w-2xl text-white rounded-md bg-blue backdrop:blur-xl hover:bg-opacity-30"
-        />
+        <Button
+          placeholder={""}
+          size="sm"
+          disabled={!email}
+          className="!absolute right-1 top-1 bg-blue disabled:!opacity-100"
+        >
+          Invite
+        </Button>
       </div>
     </div>
   );
