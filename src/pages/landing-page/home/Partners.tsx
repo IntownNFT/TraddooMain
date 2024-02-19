f9774dd2254b0a9dbfc545386fb82979de39bd8a
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-19 19:45:33 +0500

    Add new partner (fundedraffle) logo

--- src/pages/landing-page/home/Partners.tsx
+++ src/pages/landing-page/home/Partners.tsx
@@ -4,9 +4,11 @@ import Logo3 from "../../../assets/landing-page/partners/funderpro.webp";
 import Logo4 from "../../../assets/landing-page/partners/redacre.webp";
 import Logo5 from "../../../assets/landing-page/partners/propfirm.webp";
 import Logo6 from "../../../assets/landing-page/partners/cryptochill.webp";
+import Logo7 from "../../../assets/landing-page/partners/fundedraffle.webp"
+
 import { useEffect, useRef } from "react";
 
-const logos = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo6];
+const logos = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo6, Logo7];
 
 const Partners = () => {
   const logosRef = useRef<HTMLUListElement>(null);
