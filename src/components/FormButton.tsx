568b4200de9b59ce4d49cbbe615c89c79551f04e
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-03-05 00:58:16 +0500

    Add link to challenge form button

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -2,18 +2,24 @@ const FormButton = ({
   type,
   text,
   classes,
+  link,
+  target
 }: {
   type: "submit" | "reset" | "button" | undefined;
   text: string;
   classes?: string;
+  link?: string;
+  target?: string;
 }) => {
   return (
-    <button
+    <a
       type={type}
-      className={`w-full bg-blue font-rubik font-semibold text-base py-3 rounded-lg hover:bg-opacity-80 transition-all ${classes}`}
+      className={`w-full bg-blue font-rubik font-semibold text-base py-3 rounded-lg text-center hover:bg-opacity-80 transition-all ${classes}`}
+      href={link?link:""}
+      target={target}
     >
       {text}
-    </button>
+    </a>
   );
 };
 
